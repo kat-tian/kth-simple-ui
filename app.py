369e3951c21ff41dd011b2a641d207be93310984
Kat Tian <kathytian2@gmail.com>
2023-11-02 18:28:13 +0100

    update prompt and add edit box

--- app.py
+++ app.py
@@ -3,23 +3,16 @@ import requests
 
 app = Flask(__name__)
 
-conversation_history = []  # To keep track of conversation history
+conversation_history = []
 
+INITIAL_PROMPT = ("Du är en svensk mattelärare. "
+                  "Om det är en mattefråga, guida eleverna genom att ställa uppföljande frågor."
+                  "Annars, svara direkt. Alltid svara på svenska."
+                  "Undvik hälsningar.")
 
-
-
-
-INITIAL_PROMPT = ("Du är en svensk mattelärare och svarar enbart på svenska."
-                  "För mattefrågor, istället för att direkt ge svaret, guida eleverna genom att ställa uppföljande frågor för att hjälpa dem komma fram till svaret själva."
-                  "Håll ditt svar kort och inom en mening.")
-
-
-# INITIAL_PROMPT = ("Du är en svensk mattelärare och svarar enbart på svenska."
-#                   "Hjälp eleverna med deras mattefrågor genom att guida dem med stödjande och pedagogiska frågor, istället för att direkt ge svaret."
-#                   "Ditt svar ska vara koncist och korrekt, helst inom en meningar."
-#                   "Hjälp eleverna pedagogiskt utan att ge direkt svar.")
-
-MAX_TURNS = 10  # For 5 user messages and 5 assistant messages
+user_turns_since_last_prompt = 0
+MAX_TURNS_BEFORE_REPROMPT = 5
+MAX_TURNS = 10
 
 @app.route('/')
 def index():
@@ -27,22 +20,20 @@ def index():
 
 @app.route('/send_text', methods=['POST'])
 def send_text():
-    global conversation_history
+    global conversation_history, user_turns_since_last_prompt
     data = request.json
-    user_message = data["message"] + "(Som svensk mattelärare, svara på svenska. Hjälp eleverna med deras mattefrågor genom att guida dem med stödjande och pedagogiska frågor. Håll ditt svar kort, helst inom en meningar.)"  # append the initial prompt to user message
+    
+    user_message = data["message"] + " (svara ENDAST på svenska. Var koncis; svara med en eller två meningar om möjligt.)"
+    conversation_history.append({"role": "user", "content": user_message})
 
-    # If the conversation history is empty or ends with an assistant's response, just append the user message
-    if not conversation_history or conversation_history[-1]["role"] == "assistant":
-        conversation_history.append({"role": "user", "content": user_message})
-    else:
-        # If the last message was from the user, replace it with the new user message
-        conversation_history[-1] = {"role": "user", "content": user_message}
+    user_turns_since_last_prompt += 1
 
-    # If there's no system message in the conversation history, prepend it
-    if not any(message["role"] == "system" for message in conversation_history):
+    # Reprompt every MAX_TURNS_BEFORE_REPROMPT user turns
+    if user_turns_since_last_prompt >= MAX_TURNS_BEFORE_REPROMPT:
         conversation_history.insert(0, {"role": "system", "content": INITIAL_PROMPT})
+        user_turns_since_last_prompt = 0  # Reset the counter
 
-    # Send request to LLM
+    # Send request to LLM with a modified max_new_tokens
     response = requests.post(
         "https://17zayxht82.execute-api.eu-west-1.amazonaws.com/llama2-test/generate",
         headers={
@@ -51,24 +42,20 @@ def send_text():
         },
         json={
             "inputs": [conversation_history],
-            "parameters": {"max_new_tokens": 150, "temperature": 0.1}
+            "parameters": {"max_new_tokens": 80, "temperature": 0.05}  # Reduced tokens for conciseness
         }
     )
 
     response_json = response.json()
+    ai_response = response_json[0].get('generation', {}).get('content', '') if isinstance(response_json, list) and len(response_json) > 0 else ""
 
     # Append assistant's response to conversation history
-    ai_response = response_json[0].get('generation', {}).get('content', '') if isinstance(response_json, list) and len(response_json) > 0 else ""
     conversation_history.append({"role": "assistant", "content": ai_response})
 
-    # Maintain a sliding window of conversation turns
     while len(conversation_history) > MAX_TURNS:
-        conversation_history.pop(0)  # Remove the oldest turn
+        conversation_history.pop(0)  # Maintain a window of the latest turns
 
     return jsonify({"response": ai_response})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# test
